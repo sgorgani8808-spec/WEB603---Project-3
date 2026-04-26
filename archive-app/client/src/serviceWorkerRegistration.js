d4bf89b2072401a5208d271573220a8850ae9ced
sgorgani8808-spec <s.gorgani.8808@westcliff.edu>
2026-04-26 16:38:04 -0700

    fix service worker registration build error

--- archive-app/client/src/serviceWorkerRegistration.js
+++ archive-app/client/src/serviceWorkerRegistration.js
@@ -1,11 +1,3 @@
-const isLocalhost = Boolean(
-  window.location.hostname === "localhost" ||
-    window.location.hostname === "[::1]" ||
-    window.location.hostname.match(
-      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    )
-);
-
 export function register() {
   if ("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
